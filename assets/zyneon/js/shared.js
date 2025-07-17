4f2cc8e9df252d7a69119d45ba687d27c2eccce2
nerotvlive <daniel.niesmann@outlook.com>
2025-07-17 22:29:50 +0200

    Replace cookie message buttons

--- assets/zyneon/js/shared.js
+++ assets/zyneon/js/shared.js
@@ -20,7 +20,7 @@ addEventListener("DOMContentLoaded", (event) => {
         }
     }
     if(!storage) {
-        addNotification("cookie-alert","Möchtet du Cookies aktivieren?","Cookies helfen uns dabei, diese Website mit mehr Funktionalität auszustatten und dir ein besseres Webangebot zu bieten. Schaue in der Datenschutzerklärung für weitere Infos. <span class='text-bg-info'>Wenn nicht, schließe diese Nachricht einfach mit dem X.</span><br><br><a type='button' onclick='localStorage.setItem(\"enabled\",\"true\"); location.reload();' class='btn btn-primary'>Cookies aktivieren</a> <a type='button' class='btn btn-outline-light' href='https://www.nerofy.de/rechtliches' target='_blank'>Datenschutzerklärung</a>")
+        addNotification("cookie-alert","Möchtet du Cookies aktivieren?","Cookies helfen uns dabei, diese Website mit mehr Funktionalität auszustatten und dir ein besseres Webangebot zu bieten. Schaue in der Datenschutzerklärung für weitere Infos. <span class='text-bg-info'>Wenn nicht, schließe diese Nachricht einfach mit dem X.</span><br><br><button onclick='localStorage.setItem(\"enabled\",\"true\"); location.reload();' class='button-primary'>Cookies aktivieren</button> <a href='https://www.nerofy.de/rechtliches' target='_blank'><button>Datenschutzerklärung</button></a>")
     }
     initMenu();
 });
